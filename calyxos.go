c6e1dc165407ed17f46d5e2bafa9ec6f55377891
Oliver Scott <olivercscott@gmail.com>
2020-11-24 14:25:18 -0500

    GUI: Add a CalyxOS branded variant
    Change-Id: I461d89f0e379767bbae474d8184f7651f0bc6757

---
+++ calyxos.go
@@ -0,0 +1,39 @@
+//+build GUI,CalyxOS
+
+package main
+
+import (
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/container"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+	"github.com/aospalliance/device-flasher/resources/calyxos"
+	"github.com/aospalliance/device-flasher/resources/strings"
+	"net/url"
+)
+
+var redditUrl, _ = url.Parse("https://reddit.com/r/" + strings.Vendor)
+var redditLogo = &canvas.Image{Resource: calyxos.ResourceSnooSvg, FillMode: canvas.ImageFillContain}
+var redditScreenshot = &canvas.Image{Resource: calyxos.ResourceRedditPng, FillMode: canvas.ImageFillContain}
+
+func init() {
+	strings.Welcome = "Get ready for CalyxOS"
+	splashScreen = padding(5, &canvas.Image{Resource: calyxos.ResourceCalyxosPng, FillMode: canvas.ImageFillContain})
+	infoColumn = container.NewGridWithRows(2,
+		container.NewGridWithRows(3,
+			layout.NewSpacer(),
+			redditLogo,
+			container.NewCenter(
+				container.NewVBox(
+					container.NewCenter(
+						widget.NewLabel("Check out our Reddit community at"),
+					),
+					container.NewCenter(
+						widget.NewHyperlink(redditUrl.Host+redditUrl.Path, redditUrl),
+					),
+				),
+			),
+		),
+		redditScreenshot,
+	)
+}
