5c567d8013a892ebf74bc88a268874870263326b
Oliver Scott <olivercscott@gmail.com>
2021-01-13 13:31:48 -0500

    Refactor strings
    Change-Id: Icff046001fe634aecceecdcd153b351d71a902a9

---
+++ resources/strings/strings.go
@@ -0,0 +1,43 @@
+package strings
+
+var Vendor string
+
+var Title string
+var Welcome string
+
+var Back string
+var Done string
+var Finish string
+var Flash string
+var Log string
+var Next string
+var Retry string
+var Save string
+var Select string
+var View string
+
+var HeadsUp string
+
+var DeviceInstructions string
+var DeviceRebootRequired string
+var LockBootloader string
+var UnlockBootloader string
+var SetupPlatformTools string
+
+var DeveloperModeHeader string
+var DeveloperModeBody string
+var InstallHeader string
+var InstallBody string
+var OemLockHeader string
+var OemLockBody string
+var OemUnlockingHeader string
+var OemUnlockingBody string
+var PlugDeviceHeader string
+var PlugDeviceBody string
+var PrepareDeviceHeader string
+var PrepareDeviceBody string
+var SelectHeader string
+var SuccessHeader string
+var SuccessBody string
+var UsbDebuggingHeader string
+var UsbDebuggingBody string
